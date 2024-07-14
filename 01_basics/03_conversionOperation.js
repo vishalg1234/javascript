1ab22a9dafd060f0d99e9959c2b8e101d1984631
Vishal Gupta <147909073+vishalg1234@users.noreply.github.com>
2024-07-14 08:26:56 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber =  Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//conversion
+// "33" => 33
+// "33abc" =>Nan
+// true = 1;
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+
+// conversion
+// 1  = true;
+// 0 = false;
+// "" =>false
+// "vishal" = true
+
+let someNumber = 33
+
+let stringNUmber = String(someNumber);
+console.log(typeof stringNUmber)
