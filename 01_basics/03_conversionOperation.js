9704f59b0142819f99eee1522a429066e13cf980
Vishal Gupta <147909073+vishalg1234@users.noreply.github.com>
2024-07-14 08:56:31 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33";
+/*let score = "33";
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -27,3 +27,23 @@ let someNumber = 33
 
 let stringNUmber = String(someNumber);
 console.log(typeof stringNUmber)
+*/
+
+
+
+// *************************** Operations *********************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello";
+let str2 = " vishal";
+let str3 = str1 + str2;
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
