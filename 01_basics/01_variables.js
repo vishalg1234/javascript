5c331cdd4e0afe822f559bccba9b9531a03403ca
Vishal Gupta <147909073+vishalg1234@users.noreply.github.com>
2024-07-14 08:09:33 +0000

    cofig done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,5 +2,23 @@ const account_Id = 144453
 let accountEmail = "Vishal.com"
 var accountPassword = "12345"
 accountCity = "Orai"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "232323232"
+accountCity = "Bengaluru"
 
 console.log(account_Id);
+
+
+/* 
+
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("vishal")
+
+
+let name = "vishal";
+let age = 18;
+let isLoggedIn = true;
+let state = null;
+let division = 3.3;
+
+console.table([name,age,isLoggedIn,division,state])
+
+// number => 2 to power 53
+//bigint
+// string => ""
+//boolean =>true/false
+//null => standalone value
+// undefine
+// symbol => unique
+
+//object
+console.log(typeof undefined); // output undefine
+console.log(typeof null); // output object
+
