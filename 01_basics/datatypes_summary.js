1e4bb1bf1489f585ac8f0c6cd7282313204e0218
Vishal Gupta <147909073+vishalg1234@users.noreply.github.com>
2024-07-14 09:58:01 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,78 @@
+// Primitive 
+// 7 types : String  , Number, Boolean, null, undefined, Symbol, Bigint
+
+// null is a empty string or not zero value;
+const bigNumber = 2232323232322223232n;
+console.log(bigNumber);
+
+// Reference (Non Primitive)
+// Array ,Object,Functions
+
+const heros = ["shaktiman","naagraj","doga"];
+let myObj={
+    name: "Vishal",
+    age: 22,
+    
+}
+// console.log(heros)
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+
+// ************ Return type********************
+/*Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
+
+
+// *****************************************
+/*
+Primitive DataTypes:
+
+
+1.Number: Represents both integer and floating-point numbers. Example: 5, 3.14.
+ 
+2.String: Represents a sequence of characters enclosed in single or double quotes. Example: "Hello, world!".
+
+3,Boolean: Represents a binary value, either true or false, often used for conditional logic.
+
+4.Undefined: Represents a variable that has been declared but hasn't been assigned a value yet. Example: let x;.
+
+5.Null:  standalone value it is represenataion of empty value it is special type and it is a object type. Example: let y = null; 
+
+6.Symbol (ES6): it is mostly used used to find uniqnece.
+
+7.BigInt (ES11): Represents large integers that cannot be represented by the Number type.
+
+
+
+
+
+ **Refence (Object Data Types):
+
+1.Object: Represents a collection of key-value pairs (properties and methods). Example: { name: "John", age: 30 }.
+
+2.Array: Represents a list-like collection of values, indexed by numbers (integer indices). Example: [1, 2, 3, 4].
+
+3.Function: Represents a reusable block of code that can be invoked or called with arguments. Example: function add(x, y) { return x + y; }.
+
+4.Date: Represents dates and times.
+
+5.RegExp: Represents regular expressions for pattern matching.
+*/
+
+
+
